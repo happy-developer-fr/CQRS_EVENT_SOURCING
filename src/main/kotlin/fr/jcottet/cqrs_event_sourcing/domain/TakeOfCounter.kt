8264792119c97caa3f45cc56930e9ddd2cdcf7ee
jcottet <jcottet@etai.fr>
2017-11-08 16:19:52 +0100

    Gestion du pub / sub

--- src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/TakeOfCounter.kt
+++ src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/TakeOfCounter.kt
@@ -2,7 +2,7 @@ package fr.jcottet.cqrs_event_sourcing.domain
 
 class TakeOfCounter :EventSubscriber<PlaneTookOf>{
     override fun getEventClazz(): Class<PlaneTookOf> {
-        return PlaneTookOf::class.java //To change body of created functions use File | Settings | File Templates.
+        return PlaneTookOf::class.java
     }
 
     override fun handle(evt: PlaneTookOf) {
