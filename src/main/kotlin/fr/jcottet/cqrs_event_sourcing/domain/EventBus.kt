33caf35e3b627ba58551ee7f5c6c96014f0a8f46
jcottet <jcottet@etai.fr>
2017-11-09 14:42:07 +0100

    Work with infered type

--- src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/EventBus.kt
+++ src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/EventBus.kt
@@ -1,27 +1,28 @@
 package fr.jcottet.cqrs_event_sourcing.domain
 
-interface EventPublisher{
-    fun <T:DomainEvent> publish(event:T)
+interface EventPublisher {
+    fun <T : DomainEvent> publish(event: T)
 }
 
 class EventBus(val eventStream: EventStream) : EventPublisher {
-    val subscribers  = mutableListOf<IEventSubscriber>()
+    private var subscribers = mutableMapOf<Class<*>, MutableList<EventSubscriber<out DomainEvent>>>()
 
-    override fun <T:DomainEvent> publish(event:T){
+    override fun <T : DomainEvent> publish(event: T) {
         eventStream.add(event)
-        subscribers.map{it as EventSubscriber<T>}
-                .filter{it.getEventClazz().equals(event.javaClass)}
-                .forEach { it.handle(event) }
+        getListener(event.javaClass)?.forEach { it.handle(event) }
     }
 
-    fun subscribe(evtSubcriber:IEventSubscriber){
-        subscribers.add(evtSubcriber)
+    inline fun <reified T : DomainEvent> subscribe(evtSubcriber: EventSubscriber<T>) =
+            subscribe(T::class.java, evtSubcriber)
+
+    fun <T : DomainEvent> subscribe(type: Class<T>, evtSubcriber: EventSubscriber<T>) {
+        subscribers.computeIfAbsent(type) { mutableListOf() }.add(evtSubcriber)
+
     }
+
+    private fun <T : DomainEvent> getListener(type: Class<T>) = subscribers[type] as List<EventSubscriber<T>>?
 }
 
-interface IEventSubscriber
-
-interface EventSubscriber<T:DomainEvent> :IEventSubscriber{
-    fun  handle(evt:T)
-    fun getEventClazz(): Class<T>
+interface EventSubscriber<T : DomainEvent> {
+    fun handle(evt: T)
 }
--- src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/TakeOfCounter.kt
+++ src/main/kotlin/fr/jcottet/cqrs_event_sourcing/domain/TakeOfCounter.kt
@@ -1,10 +1,6 @@
 package fr.jcottet.cqrs_event_sourcing.domain
 
 class TakeOfCounter :EventSubscriber<PlaneTookOf>{
-    override fun getEventClazz(): Class<PlaneTookOf> {
-        return PlaneTookOf::class.java
-    }
-
     override fun handle(evt: PlaneTookOf) {
         count++
     }
